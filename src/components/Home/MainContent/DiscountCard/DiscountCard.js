09e79830d9a40027ef18a713ee0597e1d4d53593
Letam Bossman Barinua <letambarinua@gmail.com>
2022-12-07 11:20:46 +0100

    Add comments and change some variable names to be more descriptive

--- src/components/Home/MainContent/DiscountCard/DiscountCard.js
+++ src/components/Home/MainContent/DiscountCard/DiscountCard.js
@@ -2,6 +2,7 @@ import "./DiscountCard.css";
 import GreyStar from "../../../assets/greyStar.png";
 import GoldStar from "../../../assets/goldStar.png";
 import { discountProducts } from "./discountedProductArray";
+import uniqid from "uniqid";
 
 export default function DiscountCard() {
   const displayDiscountedProduct = discountProducts.map((product) => (
@@ -142,6 +143,7 @@ function AddToCartButton({ item, handleManipulateCartItem }) {
     <button
       className="add-to-cart"
       onClick={() => {
+        // when the button is clicked, increase the item's quantity in the cart
         handleManipulateCartItem(item, "increase");
       }}
     >
@@ -152,37 +154,44 @@ function AddToCartButton({ item, handleManipulateCartItem }) {
 
 // Display stars as rating on page
 export function StarRating({ rating }) {
-  const { returnedStar, returnedNoStar } = getGoldandGreyStarRatio(rating);
-  return (
-    <div className="star-div">
-      {returnedStar}
-      {returnedNoStar}
-    </div>
-  );
+  // Calculate the ratio of gold and grey stars
+  const starRating = getGoldAndGreyStarRatio(rating);
+
+  // Return a div containing the star rating
+  return <div className="star-div">{starRating}</div>;
 }
 
-// Function assigns the ratio between gold and grey star based on rating number
-function getGoldandGreyStarRatio(rating) {
-  const checkedStar = Math.floor(parseFloat(rating));
-  const uncheckedStar = 5 - checkedStar;
-  const starArr = duplicateStar(checkedStar, GoldStar);
-  const noStarArr = duplicateStar(uncheckedStar, GreyStar);
-  const returnedStar = starArr.map((star) => star);
-  const returnedNoStar = noStarArr.map((star) => star);
-  return { returnedStar, returnedNoStar };
+// Function that calculates the ratio of gold and grey stars based on the rating number
+function getGoldAndGreyStarRatio(rating) {
+  // Calculate the number of checked and unchecked stars
+  const checkedStarCount = Math.floor(parseFloat(rating));
+  const uncheckedStarCount = 5 - checkedStarCount;
+
+  // Create arrays of gold and grey stars
+  const goldStars = duplicateStar(checkedStarCount, GoldStar);
+  const greyStars = duplicateStar(uncheckedStarCount, GreyStar);
+
+  // Combine the arrays of gold and grey stars and return them
+  return [...goldStars, ...greyStars];
 }
 
 // Function duplicates an image based on number passed to it
 function duplicateStar(maxNumOfStar, src) {
-  const iconArr = [];
+  // Create empty array to store duplicated images
+  const iconArray = [];
+
+  // Loop through the specified number of stars
   for (let i = 1; i <= maxNumOfStar; i++) {
-    iconArr.push(
+    // Push a new image to the array, using a unique key and the provided source
+    iconArray.push(
       <img
-        key={maxNumOfStar + i}
+        key={uniqid()}
         src={src}
         alt="star"
       />
     );
   }
-  return iconArr;
+
+  // Return the array of duplicated images
+  return iconArray;
 }
